c92e3d2f153e07e6cc126841db065c181fa734ff
connor <connormccutcheon95@gmail.com>
2014-05-27 20:51:36 -0700

    forgot one thing

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,28 @@
-angular.module('app', [])
+angular.module('app', ['ngRoutes'])
+
+.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      controller:'postCtrl',
+      templateUrl:'partials/post.html'
+    })
+    .when('/post/:id', {
+      controller:'postCtrl',
+      templateUrl:'partials/post.html'
+    })
+    .when('/edit/:projectId', {
+      controller:'EditCtrl',
+      templateUrl:'partials/details.html'
+    })
+    .when('/new', {
+      controller:'createCtrl',
+      templateUrl:'partials/details.html'
+    })
+    .otherwise({
+      redirectTo:'/'
+    });
+})
+
 
 
 .controller('mainCtrl', function($scope, $http){
