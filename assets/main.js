81073d73be8c10d12687e1b3830cc35acdbdc559
connor <connormccutcheon95@gmail.com>
2014-05-27 22:39:07 -0700

    changes to the index file

--- assets/main.js
+++ assets/main.js
@@ -3,6 +3,7 @@
 angular.module('main', [])
 
 .controller('userInput', function($scope){
+  //Microphone handler
   var mic = new Wit.Microphone($scope.microphone);
   var micRunning = false;
   mic.onaudiostart = function () {
@@ -13,8 +14,24 @@ angular.module('main', [])
     console.log("mic stopped");
     micRunning = false;
   };
-
+  mic.onresult = function (intent, entities) {
+    executeCommand();
+  };
   mic.connect("O5OAGEKON63WTIBJTHRD3SXOOTJEZWSV");
+  //input field handler
+  $scope.typeAction = function(event){
+    if(event.keyCode != 13) return;
+    executeCommand($scope.input);
+
+  };
+
+  var executeCommand = function(input){
+    $scope.output = input;
+
+  }
+
+
+
   $scope.startMic = function(){
     if(!micRunning)
       mic.start();
