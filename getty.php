643b1d1ab0ce2e0ecb3aee56071fdf49b2a3df47
connor <connormccutcheon95@gmail.com>
2014-05-28 09:20:04 -0700

    cvklsdlk

--- getty.php
+++ getty.php
@@ -13,7 +13,7 @@ $options = array(
 $context  = stream_context_create($options);
 $result = file_get_contents($url, false, $context);
 
-var_dump($result['access_token']);
+var_dump($result);
 // search string, let's look up "tree"
 $searchPhrase = "tree";
 
