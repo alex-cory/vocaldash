c2a367d959ffa2c7b5a58ffcd2f0c14a6db3af54
connor <connormccutcheon95@gmail.com>
2014-05-27 22:06:20 -0700

    for dave

--- assets/main.js
+++ assets/main.js
@@ -4,13 +4,22 @@ angular.module('main', [])
 
 .controller('userInput', function($scope){
   var mic = new Wit.Microphone($scope.microphone);
+  var micRunning = false;
   mic.onaudiostart = function () {
-    console.log("micSTarted");
+    console.log("mic started");
+    micRunning = true;
   };
+  mic.onaudioend = function () {
+    console.log("mic stopped");
+    micRunning = false;
+  };
+
   mic.connect("O5OAGEKON63WTIBJTHRD3SXOOTJEZWSV");
   $scope.startMic = function(){
-    mic.start();  
-
+    if(!micRunning)
+      mic.start();
+    else
+      mic.stop();
   };
 
 })
