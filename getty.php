c460637343a29529582c2e05c51d6a60b4d810f2
connor <connormccutcheon95@gmail.com>
2014-05-28 09:31:47 -0700

    slashes

--- getty.php
+++ getty.php
@@ -14,7 +14,7 @@ $context  = stream_context_create($options);
 $result = file_get_contents($url, false, $context);
 
 var_dump($result);
-$token = json_encode($result);
+$token = stripslashes(json_encode($result));
 print_r($token);
 
 // search string, let's look up "tree"
